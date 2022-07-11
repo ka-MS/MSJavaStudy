ebb4037c5b03bf16d02c1d391282131985da17f3
HMS <HMS@DESKTOP-3CDT6EB>
2022-07-11 14:33:54 +0900

    변경

--- src/emp/run/TestEmp.java
+++ src/emp/run/TestEmp.java
@@ -7,7 +7,7 @@ public class TestEmp {
 		Employee employee = new Employee();// 관계를 맺음 cntl+space 하면 임폴트됨
 		employee.printEmployee(); // 타자느리니까 같은거 반복연습하자
 		// (관계맺)사용할수 있는 상태가 됨-> 틀을 불러올수있다 ->명령어를 실행할수 있다.
-		
+		System.out.println();
 	}
 
 }
